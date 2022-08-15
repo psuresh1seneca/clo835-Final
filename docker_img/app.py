a9ccc83ad1caf46680ef05b184b6700b49d8d086
Praveen Suresh <103491779+psuresh1seneca@users.noreply.github.com>
2022-08-14 23:05:56 -0400

    Update app.py

--- docker_img/app.py
+++ docker_img/app.py
@@ -27,11 +27,11 @@ def main():
         color = '#ff3f3f'
         err_message = str(e)
 
-return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, groupname=groupname, successimg=successimg, failedimg=failedimg)
+return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + "; groupname=" + (os.environ.get('groupname') + "; successimg=" + (os.environ.get('successimg') + "; failedimg=" + (os.environ.get('failedimg') + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color)
 @app.route("/debug")
 def debug():
     color = '#2196f3'
-    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; groupname=" + (os.environ.get('groupname')  or "Not Set") + "; success_image=" + (os.environ.get('successimg')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set"), color=color)
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; groupname=" + (os.environ.get('groupname') + "; successimg=" + (os.environ.get('successimg') + "; failedimg=" + (os.environ.get('failedimg') + " + " +; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set"), color=color)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81)
