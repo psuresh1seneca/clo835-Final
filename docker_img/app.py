33851c4efce4f184a5ba414d8055a4573a9c0724
bibinmekkattukunnil-biju <103480551+bibinmekkattukunnil-biju@users.noreply.github.com>
2022-08-14 22:37:25 -0400

    adding provision for env variables

--- docker_img/app.py
+++ docker_img/app.py
@@ -32,7 +32,7 @@ def main():
 @app.route("/debug")
 def debug():
     color = '#2196f3'
-    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set"), color=color)
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; groupname=" + (os.environ.get('groupname')  or "Not Set") + "; success_image=" + (os.environ.get('successimg')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set"), color=color)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81)
