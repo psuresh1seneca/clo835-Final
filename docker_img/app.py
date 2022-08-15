59a7ae9f1227030e047a78d36d5df02557ebd014
bibinmekkattukunnil-biju <103480551+bibinmekkattukunnil-biju@users.noreply.github.com>
2022-08-14 23:26:50 -0400

    Update app.py

--- docker_img/app.py
+++ docker_img/app.py
@@ -27,7 +27,7 @@ def main():
         color = '#ff3f3f'
         err_message = str(e)
 
-return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; groupname=" + (os.environ.get('groupname')) + "; successimg=" + (os.environ.get('successimg'))+ "; failedimg=" + (os.environ.get('failedimg')) + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color)
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; groupname=" + (os.environ.get('groupname')) + "; successimg=" + (os.environ.get('successimg'))+ "; failedimg=" + (os.environ.get('failedimg')) + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color)
 @app.route("/debug")
 def debug():
     color = '#2196f3'
